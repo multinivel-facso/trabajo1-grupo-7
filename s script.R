8c6435d86abd8d6c7ae7d9aed54f31e38ef98b63
matyduhh <matilda.raccagni@ug.uchile.cl>
2025-05-25 15:02:49 -0400

    cargar librerias y cargar base de datos

--- s script.R
+++ s script.R
@@ -1,1 +1,26 @@
-#prueba 1
+#25/05/2025
+
+#Inicio de trabajo
+
+#Cargamos las librerias
+pacman::p_load(tidyverse, # Manipulacion de datos
+               car, # Recodificar
+               sjPlot, # Tablas y graficos
+               sjmisc, # Descriptivos
+               kableExtra, # Tablas
+               psych, # Bivariados
+               corrplot, # Graficos correlación
+               broom, 
+               gginference,
+               ggplot2,
+               dplyr,
+               stargazer,
+               lme4,
+               haven) # Varios
+
+options(scipen = 999) # para desactivar notacion cientifica
+rm(list = ls()) # para limpiar el entorno de trabajo
+
+#Cargar base de datos
+load(url("https://datos.gob.cl/dataset/c6983439-49f6-4e71-85fe-e8de6e73dae0/resource/ed81f50c-1c7d-43d9-9083-dfc161e0cd66/download/20240516_enssex_data.rdata"))
+
