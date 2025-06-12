2a2d8e0fadc9eb5ff607a2096598129a7f78a8f1
rodriwo <rdgohc@gmail.com>
2025-06-12 16:21:12 -0400

    Pendiente aleatoria + hipótesis 5 + no me acuerdo q mas

--- Entrega_2_correcciones.R
+++ Entrega_2_correcciones.R
@@ -17,24 +17,27 @@ pacman::p_load(tidyverse, # Manipulacion de datos
                haven,
                reghelper,
                texreg, 
-               gt) # Varios
+               gt,
+               ggeffects,
+               labelled) # Varios
 
 options(scipen = 999) # para desactivar notacion cientifica
 rm(list = ls()) # para limpiar el entorno de trabajo
 
 load("casen.Rdata") #cargamos base de datos
 
-# 1) Ver descriptivos en una única tabla
+attach(casen)
 
+# 1) Ver descriptivos en una única tabla
 
 # Descriptivos variables de nivel 1
 casen_desc1 = casen %>% select(nvl_educ, pueblo_indigena,
                               dificultad_conc, comuna)
 
-casen_desc1$comuna_factor <- as_factor(casen$comuna)
+casen$comuna_factor <- as_factor(casen$comuna)
 
 casen_desc1 %>% ungroup() %>%
-  select(-comuna, -comuna_factor) %>%
+  select(-comuna) %>%
   psych::describe() %>%
   select(n, mean, sd, median, min, max, range, median) %>%
   kable(
@@ -59,8 +62,8 @@ casen_desc2 %>% ungroup() %>%
   ) %>%
   kable_styling(full_width = TRUE)
 
-# Tabla de descriptivos de la variable cluster
-tabla_cluster <- casen_desc1 %>%
+# Tabla de descriptivos de la variable cluster AGREGAR A LA TABLA DE DESCRIPTIVOS DE NIVEL 2
+tabla_cluster <- casen %>%
   count(comuna_factor, name = "N")
 
 tabla_cluster %>% ungroup() %>%
@@ -76,10 +79,45 @@ tabla_cluster %>% ungroup() %>%
 
 # 2) Resultados
 
+# MODELO MULTINIVEL
+resultados_1 <- lmer(nvl_educ ~ pueblo_indigena + dificultad_conc + 
+                    mean_educ_padres + mean_conectividad + 
+                    (1 | comuna), data = casen)
+
+screenreg(resultados_1)
+
+tab_model(resultados_1,
+          show.ci = FALSE,
+          show.icc = FALSE,
+          title = "Modelo multinivel",
+          dv.labels = c("Nivel educacional máximo"),
+          pred.labels = c("(Intercepto)", 
+                          "Pertenencia a pueblo indígena", 
+                          "Dificultad para concentrarse", 
+                          "Promedio educativo de los padres (comuna)", 
+                          "Promedio conectividad (comuna)"))
 
 
 
+  # 3) Efecto aleatorio 
+
+# Pendiente aleatoria para pueblo_indigena
+reg_al1=lmer(nvl_educ ~ 1 + pueblo_indigena + dificultad_conc + mean_conectividad + 
+               mean_educ_padres + ( 1 + pueblo_indigena | comuna), data = casen)
 
 
+tab_model(resultados_1, reg_al1,
+          show.ci = FALSE,
+          show.icc = FALSE,
+          title = "Comparación de modelos",
+          dv.labels = c("Modelo multinivel",
+                        "Modelo con pendiente aleatoria"),
+          pred.labels = c("(Intercepto)", 
+                          "Pertenencia a pueblo indígena", 
+                          "Dificultad para concentrarse", 
+                          "Promedio educativo de los padres (comuna)", 
+                          "Promedio conectividad (comuna)"))
+
+anova(resultados_1, reg_al1)
 
 
